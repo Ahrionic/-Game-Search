aabe9f465843984e614e41abd76aeb97b58cc673
Korey Luu <39642290+Ahrionic@users.noreply.github.com>
2022-08-30 13:51:17 -0700

    client side template

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -13,13 +13,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
